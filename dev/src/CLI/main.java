7388095c04f621c6f4afd1810417fbf5c14309d9
michael <amarmic@post.bgu.ac.il>
2020-04-08 02:11:34 +0300

    check

--- dev/src/CLI/main.java
+++ dev/src/CLI/main.java
@@ -5,6 +5,6 @@ public class main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!");
-        System.out.println("check234");
+        System.out.println("check234567");
 	}
 }
