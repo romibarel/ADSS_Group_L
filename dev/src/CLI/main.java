1aebd65cfbcb3948d592ded374de817c1aa043d4
michael <amarmic@post.bgu.ac.il>
2020-04-08 01:57:27 +0300

    "check commit"

--- dev/src/CLI/main.java
+++ dev/src/CLI/main.java
@@ -5,6 +5,6 @@ public class main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!");
-        System.out.println("check2");
+        System.out.println("check23");
 	}
 }
