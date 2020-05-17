f394b6d0d310ba5a29acf661cfecaa786c359e46
Haim <48633669+HaimTempo@users.noreply.github.com>
2020-05-17 20:01:06 +0300

    haim62

--- dev/src/Presentation/PTIDelController.java
+++ dev/src/Presentation/PTIDelController.java
@@ -28,6 +28,24 @@ public class PTIDelController {
     }
 
     public void setup(){
+        PTIDelController pti = PTIDelController.getPTI();
+        ITPDelController itp = ITPDelController.getITP();
+        ITBDelController itb = ITBDelController.getITB();
+        BTIController bti = BTIController.getBTI();
+        BTDController btd = BTDController.getBTD();
+        DALController dtb = DALController.getDTB();
+
+        List<String[]> sections = new LinkedList<>();
+        List<String[]> locations = new LinkedList<>();
+        List<String[]> trucks = new LinkedList<>();
+
+        pti.set();
+        itp.set();
+        itb.set();
+        bti.set(sections, locations, trucks);
+    }
+
+    public void oldSetup(){
         boolean finish = false;
         PTIDelController pti = PTIDelController.getPTI();
         ITPDelController itp = ITPDelController.getITP();
