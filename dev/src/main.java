aca2ec5d44de61080cc8a367153be731a9a25eff
romi barel <romibar@post.bgu.ac.il>
2020-05-17 16:01:15 +0300

    yay lets change everythinggggggggggggg

--- dev/src/main.java
+++ dev/src/main.java
@@ -6,30 +6,24 @@ import java.util.Scanner;
 public class main {
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
-        int choice=0;
+        String choice;
         boolean quit=false;
         while(!quit){
             System.out.println("Hello, please choose the system you want to use:");
             System.out.println("1. Delivery System");
             System.out.println("2. HR System");
             System.out.println("3. Exit");
-            try {
-                choice = Integer.parseInt(input.nextLine());
-            }
-            catch (Exception e){
-                System.out.println("illegal input");
-                continue;
-            }
+            choice = input.nextLine();
             switch (choice){
-                case 1:
+                case "1":
                     PTIDelController pti = PTIDelController.getPTI();
                     pti.setup();
                     pti.start();
                     break;
-                case 2:
+                case "2":
                     WorkerMenu.mainFunc();
                     break;
-                case 3:
+                case "3":
                     quit=true;
                     break;
                 default:
